de2d4e46df9d82ecb9687d5f42477af5c142132f
akshay-john <155622631+akshay-john@users.noreply.github.com>
2024-01-25 14:32:52 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, num3, sum;
+    int num1, num2, num3, num4, sum;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -12,10 +12,14 @@ int main() {
      printf("Enter the third number: ");
     scanf("%d", &num3);
     
+    printf("Enter the fourth number: ");
+    scanf("%d", &num4);
     
-    sum = num1 + num2 + num3;
     
-    printf("The sum of %d and %d is %d.", num1, num2, num3, sum);
+    
+    sum = num1 + num2 + num3 +num4;
+    
+    printf("The sum of %d and %d is %d.", num1, num2, num3, num4, sum);
     
     return 0;
 }
